d370ca08c4f59202d913485aa66a17b7311a84c4
ryota-09 <n.ryo950911@gmail.com>
2022-07-06 10:40:19 +0900

    createUserロジック記述

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import * as bcrypt from 'bcrypt';
+
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserInput } from './dto/createUserInput.dto';
+import { User } from '@prisma/client';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createNewUser({
+    username,
+    email,
+    password,
+  }: CreateUserInput): Promise<User> {
+    const salt = await bcrypt.genSalt();
+    const newUser = await this.prisma.user.create({
+      data: {
+        id: uuid(),
+        username: username,
+        email: email,
+        password: await bcrypt.hash(password, salt),
+        img_path: '',
+        created_at: new Date(),
+        updated_at: null,
+      },
+    });
+    return newUser;
+  }
+  //   async findFirst(args: FindFirstUserArgs): Promise<User | null> {
+  //     return this.prisma.user.findFirst(args);
+  // }
+}
