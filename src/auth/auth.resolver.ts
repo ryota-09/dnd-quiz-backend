9d2bf97453e0c817e967f3cc26b4ae4a2c4fd81f
ryota-09 <n.ryo950911@gmail.com>
2022-07-07 10:49:49 +0900

    authresolverの編集

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -4,6 +4,7 @@ import { AuthService } from './auth.service';
 import { LoginResponse } from './dto/loginResponse.dto';
 import { GqlAuthGuard } from './guards/gql-auth.guard';
 import { LoginUserInput } from './dto/loginUserInput.dto';
+import { JwtRefreshAuthGuard } from './guards/jwt-refresh-auth.guard';
 
 @Resolver()
 export class AuthResolver {
@@ -16,5 +17,20 @@ export class AuthResolver {
     @Context() context,
   ) {
     return this.authService.login(context.user);
+  }
+
+  @Mutation(() => LoginResponse)
+  @UseGuards(JwtRefreshAuthGuard)
+  async refreshToken(@Context() context) {
+    return this.authService.refreshToken(
+      context.req.user,
+      context.req.headers.authorization,
+    );
+  }
+
+  @Mutation(() => Boolean)
+  @UseGuards(JwtRefreshAuthGuard)
+  async logout(@Context() context) {
+    return this.authService.logout(context.req.user);
   }
 }
