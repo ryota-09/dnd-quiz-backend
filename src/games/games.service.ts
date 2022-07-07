0f005d58a8dda92b4150414137660f37e6391c9c
ryota-09 <n.ryo950911@gmail.com>
2022-07-07 17:40:37 +0900

    createNewGameメソッド作成

--- src/games/games.service.ts
+++ src/games/games.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateGameInput } from './dto/createGameInput.dto';
 import { Game } from './games.entity';
 
 @Injectable()
@@ -8,5 +11,20 @@ export class GamesService {
 
   async findAllGames(): Promise<Game[]> {
     return await this.prisma.game.findMany();
+  }
+
+  async createNewGame(createGameInput: CreateGameInput): Promise<Game> {
+    const newGame = await this.prisma.game.create({
+      data: {
+        id: uuid(),
+        user_id: createGameInput.user_id,
+        trial_time: createGameInput.trial_time,
+        correct_count: createGameInput.correct_count,
+        vocabulary_point: createGameInput.vocabulary_point,
+        total_point: createGameInput.total_point,
+        created_at: new Date(),
+      },
+    });
+    return newGame;
   }
 }
